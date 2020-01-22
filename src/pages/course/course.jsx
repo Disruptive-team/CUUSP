54b2994c2618a714a9931cb72c869baf8e51aa13
lcgzyd <3010436802@qq.com>
2020-01-22 23:06:06 +0800

    解决固定定位高度获取bug，增加左侧时间节次

--- src/pages/course/course.jsx
+++ src/pages/course/course.jsx
@@ -1,6 +1,5 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
-import classNames from 'classnames'
+import {View, ScrollView, Text} from '@tarojs/components'
 
 import './course.css'
 import Calendar from '../../components/calendar/Calendar'
@@ -40,8 +39,26 @@ class Course extends Component {
     for(let i=1;i<25;i++) {
       week_num.push(i)
     }
-    console.log(week_num)
     this.setState({week_num})
+    this.getTopHeight()
+  }
+
+  getTopHeight () {
+    Taro.createSelectorQuery().select('#position_fixed').fields({
+      dataset: true,
+      size: true
+    }, res => {
+      console.log(res.height)
+      this.setState({
+        margin_top: res.height+2
+      })
+    }).exec()
+  }
+
+  selectWeek () {
+    this.setState({
+      arrow_up: !this.state.arrow_up
+    })
   }
 
   state = {
@@ -58,28 +75,46 @@ class Course extends Component {
       week: '01-13',
       section_length: '1-4',
     }],
-    arrow_up: true,
-    week_num: []
+    arrow_up: false,
+    week_num: [],
+    margin_top: 64,
+    left_data: [1, 2, 3, 4, '中\n午', 5, 6, 7, 8, '晚\n上', 9, 10, 11, 12]
   }
 
   render () {
     return (
       <View className='course'>
-        <View className='top'>
-          <View className='iconfont' style='font-size: 16px;padding: 8px 8px'>&#xe6cd;</View>
-          {this.state.arrow_up && <View className='iconfont' style='font-size: 16px;padding: 8px 8px'>第 10 周 &#xe6b9;</View>}
-          {!this.state.arrow_up && <View className='iconfont' style='font-size: 16px;padding: 8px 8px'>第 10 周 &#xe797;</View>}
-          <View className='iconfont' style='font-size: 16px;padding: 8px 8px'>&#xe623;</View>
+        <View className='position_fixed' id='position_fixed'>
+          <View className='top'>
+            <View className='iconfont' style='font-size: 16px;padding: 8px 8px'>&#xe6cd;</View>
+            {this.state.arrow_up && <View onClick={this.selectWeek} className='iconfont' style='font-size: 16px;padding: 8px 8px'>第 10 周 &#xe797;</View>}
+            {!this.state.arrow_up && <View onClick={this.selectWeek} className='iconfont' style='font-size: 16px;padding: 8px 8px'>第 10 周 &#xe6b9;</View>}
+            <View className='iconfont' style='font-size: 16px;padding: 8px 8px'>&#xe623;</View>
+          </View>
+          <View className='week'>
+            {this.state.arrow_up &&
+              <ScrollView className='scroll_view' scrollX='false'>
+                {this.state.week_num.map((item, index) => {
+                  return (<View key={index} className='week_item'>
+                      <Text style='display: inline'>{item}</Text>
+                  </View>)
+                })}
+              </ScrollView>}
+          </View>
+          <Calendar week='3' start_date='2020-01-06' />
         </View>
-        <View className='week'>
-          {this.state.arrow_up && <View className='week_inner'>{
-            this.state.week_num.map((item, index) => {
-              return (<View key={index}>{item}</View> )
-            })
-          }</View>}
+        <View className='course-content' style={'margin-top: '+this.state.margin_top+'px'}>
+          <View className='left-section'>
+            <View className='left-section-wrapper'>
+              {this.state.left_data.map((item, index) => {
+                return (<View key={index}>{item}</View>)
+              })}
+            </View>
+          </View>
+          <View className='course-content'>
+            <CourseComponent bg_color='pink' course_data={this.state.course_d} />
+          </View>
         </View>
-        <Calendar week='3' start_date='2020-01-06' />
-        <CourseComponent bg_color='pink' course_data={this.state.course_d} />
       </View>
     )
   }
